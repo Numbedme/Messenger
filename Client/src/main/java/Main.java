bd612ecba5ef248eb4eb68cf87d58d922ea18138
k.tokarev <k.tokarev@itfbgroup.ru>
2016-11-22 12:11:10 +0300

    changes

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -10,44 +10,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     public static void main(String[] args) {
-
         launch(args);
-        /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.println("Enter user name");
-            String name = reader.readLine();
-            System.out.println("Enter host name");
-            String host = reader.readLine();
-            System.out.println("Enter port");
-            String port = reader.readLine();
-
-            Socket s = new Socket(host, Integer.parseInt(port));
-            ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(s.getInputStream());
-
-            out.writeObject(name);
-            out.flush();
-
-            Thread t = new Thread(() -> {
-                while (true) {
-                    try {
-                        Message m = (Message) in.readObject();
-                        System.out.println(m.toString());
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            t.start();
-
-            while (true){
-                String text = reader.readLine();
-                out.writeObject(new Message(text, name));
-                out.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
     }
 
     @Override
